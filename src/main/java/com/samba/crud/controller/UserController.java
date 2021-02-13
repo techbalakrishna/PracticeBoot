96ec08d4f1cd7e966dc939a04e6bb314b3c23418
balu <tech.balakrishna3321@gmail.com>
2021-02-13 13:53:07 +0530

    user contorller modified

--- src/main/java/com/samba/crud/controller/UserController.java
+++ src/main/java/com/samba/crud/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.samba.crud.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.samba.crud.model.User;
+import com.samba.crud.service.UserService;
 
 @RestController
 public class UserController {
-
+private UserService service;
+public User createUser(@RequestBody User user) {
+	return service.createUser(user);
+}
 }
